a0531bc989218792216cb123886be7e12fe1ce4a
Wei <wayli@umich.edu>
2019-01-15 17:04:29 -0500

    setting up js

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,9 @@
+var Person = require('./modules/Person');
+
+alert("13");
+
+var wei = new Person("wei", "blue");
+wei.greet();
+
+var li = new Person("li", "green");
+li.greet();
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -17,9 +17,17 @@ gulp.task('watch',function(){
     watch('./app/assets/styles/**/*.css', function(){
         gulp.start('cssInject');
     });
+
+    watch('./app/assets/scripts/**/*.js', function(){
+        gulp.start('scriptsRefresh');
+    })
 });
 
 gulp.task('cssInject',['styles'], function(){
     return gulp.src('.app/temp/styles.style.css')
     .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function(){
+    browserSync.reload();
 });
