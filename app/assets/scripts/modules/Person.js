9e9b4791282997f80bfcdf9134e641d772390853
Wei <wayli@umich.edu>
2019-01-16 13:45:00 -0500

    mobile menu

--- app/assets/scripts/modules/Person.js
+++
@@ -1,9 +0,0 @@
-function Person(name, favColor){
-    this.name = name;
-    this.favColor = favColor;
-    this.greet = function(){
-        console.log("He there! my name is " +this.name+ " and my fav color is " + this.favColor+ ".");
-    }
-}
-
-module.exports = Person;
