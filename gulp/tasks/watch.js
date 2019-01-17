580e27b3deb3e6f9cd8c5a8af6fbbc461ce65838
Wei <wayli@umich.edu>
2019-01-17 15:38:21 -0500

    project voco

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -14,6 +14,10 @@ gulp.task('watch',function(){
         browserSync.reload();
     });
 
+    watch('./app/voco.html', function(){
+        browserSync.reload();
+    });
+
     watch('./app/assets/styles/**/*.css', function(){
         gulp.start('cssInject');
     });
