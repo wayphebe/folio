32d37f5e61f1ad1aeeed295ec5c9eabced627978
Wei <wayli@umich.edu>
2019-01-17 23:30:31 -0500

    publish

---
+++ gulp/tasks/build.js
@@ -0,0 +1,59 @@
+var gulp = require('gulp'),
+imagemin = require('gulp-imagemin'),
+del =require('del'),
+usemin =require('gulp-usemin'),
+rev =require('gulp-rev'),
+cssnano = require('gulp-cssnano'),
+uglify = require('gulp-uglify'),
+browserSync = require('browser-sync').create();
+
+
+gulp.task('previewDist', function(){
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "docs"
+        }
+    });
+});
+
+gulp.task('deleteDistFolder',  function(){
+    return del("./docs");  
+});
+
+gulp.task('copyGeneralFiles', ['deleteDistFolder'], function() {
+    var pathsToCopy = [
+      './app/**/*',
+      '!./app/index.html',
+      '!./app/assets/images/**',
+      '!./app/assets/styles/**',
+      '!./app/assets/scripts/**',
+      '!./app/temp',
+      '!./app/temp/**'
+    ]
+  
+    return gulp.src(pathsToCopy)
+      .pipe(gulp.dest("./docs"));
+  });
+
+
+gulp.task('optimizeImages', ['deleteDistFolder'], function(){
+    return gulp.src(['./app/assets/images/**/*'])
+    .pipe(imagemin({
+        progressive:true,
+        interlaced: true,
+        multipass:true
+    }))
+    .pipe(gulp.dest("./docs/assets/images"));
+});
+
+gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], function(){
+    return gulp.src("./app/index.html")
+    .pipe(usemin({
+        css: [function(){return rev()}, function(){return cssnano()}],
+        js: [function(){return rev()}, function(){return uglify()}]
+    }))
+    .pipe(gulp.dest("./docs"));
+});
+
+gulp.task('build',['deleteDistFolder', 'copyGeneralFiles', 'optimizeImages', 'usemin']);
